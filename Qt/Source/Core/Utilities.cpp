398d6d79ae73e2010a5a00e819c7b5596745c1d8
blurryroots <blurryroots@posteo.de>
2023-09-21 00:13:43 +0200

    cleared comments in top level

--- Qt/Source/Core/Utilities.cpp
+++ Qt/Source/Core/Utilities.cpp
@@ -13,26 +13,33 @@
 namespace Utility {
 
 void ApplyDarkMode (QApplication *app) {
+	static QColor A 	( 53,  53,  53);
+	static QColor B 	( 25,  25,  25);
+	static QColor C 	(128,  13, 128);
+	static QColor Black ( 13,   7,  13);
+	static QColor Grey 	( 64,  60,  64);
+	static QColor White (248, 255, 248);
+
 	QPalette palette;
-	palette.setColor(QPalette::Window, QColor(53, 53, 53));
-	palette.setColor(QPalette::WindowText, Qt::white);
-	palette.setColor(QPalette::Base, QColor(25, 25, 25));
-	palette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
-	palette.setColor(QPalette::ToolTipBase, Qt::black);
-	palette.setColor(QPalette::ToolTipText, Qt::white);
-	palette.setColor(QPalette::Text, Qt::white);
-	palette.setColor(QPalette::Button, QColor(53, 53, 53));
-	palette.setColor(QPalette::ButtonText, Qt::white);
-	palette.setColor(QPalette::BrightText, Qt::red);
-	palette.setColor(QPalette::Link, QColor(128, 13, 128));
-	palette.setColor(QPalette::Highlight, QColor(128, 13, 128));
-	palette.setColor(QPalette::HighlightedText, Qt::black);
-	palette.setColor(QPalette::Active, QPalette::Button, QColor(53, 53, 53));
-    palette.setColor(QPalette::Disabled, QPalette::ButtonText, Qt::darkGray);
-    palette.setColor(QPalette::Disabled, QPalette::WindowText, Qt::darkGray);
-    palette.setColor(QPalette::Disabled, QPalette::Text, Qt::darkGray);
-    palette.setColor(QPalette::Disabled, QPalette::Light, QColor(53, 53, 53));
-	app->setPalette(palette);
+	palette.setColor (QPalette::Window,								A);
+	palette.setColor (QPalette::WindowText,							White);
+	palette.setColor (QPalette::Base,								B);
+	palette.setColor (QPalette::AlternateBase,						A);
+	palette.setColor (QPalette::ToolTipBase,						Black);
+	palette.setColor (QPalette::ToolTipText,						White);
+	palette.setColor (QPalette::Text,								White);
+	palette.setColor (QPalette::Button,								A);
+	palette.setColor (QPalette::ButtonText,							White);
+	palette.setColor (QPalette::BrightText,							Qt::red);
+	palette.setColor (QPalette::Link,								C);
+	palette.setColor (QPalette::Highlight,							C);
+	palette.setColor (QPalette::HighlightedText,					Black);
+	palette.setColor (QPalette::Active, 	QPalette::Button,		A);
+    palette.setColor (QPalette::Disabled, 	QPalette::ButtonText,	Grey);
+    palette.setColor (QPalette::Disabled, 	QPalette::WindowText,	Grey);
+    palette.setColor (QPalette::Disabled, 	QPalette::Text,			Grey);
+    palette.setColor (QPalette::Disabled, 	QPalette::Light,		A);
+	app->setPalette (palette);
 }
 
 QJsonObject MergeJsonObjects (const QJsonObject &a, const QJsonObject &b) {
